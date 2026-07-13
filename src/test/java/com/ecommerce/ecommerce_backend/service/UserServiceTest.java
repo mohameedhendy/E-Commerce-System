a0d89d97ccb3b0bf82bd7e18796099c89d40b8c3
Mohamed Hendy <midohendy48@gmail.com>
2026-07-14 01:01:56 +0300

    test: improve user service test isolation

--- src/test/java/com/ecommerce/ecommerce_backend/service/UserServiceTest.java
+++ src/test/java/com/ecommerce/ecommerce_backend/service/UserServiceTest.java
@@ -5,7 +5,12 @@ import com.ecommerce.ecommerce_backend.dao.VerificationTokenDAO;
 import com.ecommerce.ecommerce_backend.dto.LoginBody;
 import com.ecommerce.ecommerce_backend.dto.PasswordResetBody;
 import com.ecommerce.ecommerce_backend.dto.RegistrationBody;
-import com.ecommerce.ecommerce_backend.exception.*;
+import com.ecommerce.ecommerce_backend.exception.EmailFailureException;
+import com.ecommerce.ecommerce_backend.exception.EmailNotFoundException;
+import com.ecommerce.ecommerce_backend.exception.InvalidCredentialsException;
+import com.ecommerce.ecommerce_backend.exception.InvalidTokenException;
+import com.ecommerce.ecommerce_backend.exception.UserAlreadyExistException;
+import com.ecommerce.ecommerce_backend.exception.UserNotVerifiedException;
 import com.ecommerce.ecommerce_backend.model.LocalUser;
 import com.ecommerce.ecommerce_backend.model.VerificationToken;
 import com.icegreen.greenmail.configuration.GreenMailConfiguration;
@@ -13,13 +18,14 @@ import com.icegreen.greenmail.junit5.GreenMailExtension;
 import com.icegreen.greenmail.util.ServerSetupTest;
 import jakarta.mail.Message;
 import jakarta.mail.MessagingException;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @SpringBootTest
@@ -30,9 +36,14 @@ public class UserServiceTest {
      * Extension for mocking email sending.
      */
     @RegisterExtension
-    private static GreenMailExtension greenMailExtension = new GreenMailExtension(ServerSetupTest.SMTP)
-            .withConfiguration(GreenMailConfiguration.aConfig().withUser("springboot", "secret"))
-            .withPerMethodLifecycle(true);
+    private static final GreenMailExtension greenMailExtension =
+            new GreenMailExtension(ServerSetupTest.SMTP)
+                    .withConfiguration(
+                            GreenMailConfiguration
+                                    .aConfig()
+                                    .withUser("springboot", "secret")
+                    )
+                    .withPerMethodLifecycle(true);
 
     @Autowired
     private UserService userService;
@@ -52,29 +63,58 @@ public class UserServiceTest {
     @Test
     @Transactional
     public void testRegisterUser() throws MessagingException {
+
         RegistrationBody body = new RegistrationBody();
         body.setUsername("UserA");
-        body.setEmail("UserServiceTest$testRegisterUser@junit.com");
+        body.setEmail(
+                "UserServiceTest$testRegisterUser@junit.com"
+        );
         body.setFirstName("FirstName");
         body.setLastName("LastName");
         body.setPassword("MySecretPassword123");
-        Assertions.assertThrows(UserAlreadyExistException.class,
-                () -> userService.registerUser(body), "Username should already be in use.");
-        body.setUsername("UserServiceTest$testRegisterUser");
+
+        Assertions.assertThrows(
+                UserAlreadyExistException.class,
+                () -> userService.registerUser(body),
+                "Username should already be in use."
+        );
+
+        body.setUsername(
+                "UserServiceTest$testRegisterUser"
+        );
         body.setEmail("UserA@junit.com");
-        Assertions.assertThrows(UserAlreadyExistException.class,
-                () -> userService.registerUser(body), "Email should already be in use.");
-        body.setEmail("UserServiceTest$testRegisterUser@junit.com");
-        Assertions.assertDoesNotThrow(() -> userService.registerUser(body),
-                "User should register successfully.");
-        Assertions.assertEquals(body.getEmail(), greenMailExtension.getReceivedMessages()[0]
-                .getRecipients(Message.RecipientType.TO)[0].toString());
+
+        Assertions.assertThrows(
+                UserAlreadyExistException.class,
+                () -> userService.registerUser(body),
+                "Email should already be in use."
+        );
+
+        body.setEmail(
+                "UserServiceTest$testRegisterUser@junit.com"
+        );
+
+        Assertions.assertDoesNotThrow(
+                () -> userService.registerUser(body),
+                "User should register successfully."
+        );
+
+        Assertions.assertEquals(
+                body.getEmail(),
+                greenMailExtension
+                        .getReceivedMessages()[0]
+                        .getRecipients(
+                                Message.RecipientType.TO
+                        )[0]
+                        .toString()
+        );
     }
 
     @Test
     @Transactional
     public void testLoginUser()
-            throws UserNotVerifiedException, EmailFailureException {
+            throws UserNotVerifiedException,
+            EmailFailureException {
 
         LoginBody body = new LoginBody();
 
@@ -103,7 +143,9 @@ public class UserServiceTest {
 
         Assertions.assertEquals(
                 1,
-                greenMailExtension.getReceivedMessages().length
+                greenMailExtension
+                        .getReceivedMessages()
+                        .length
         );
 
         UserNotVerifiedException secondAttemptException =
@@ -120,98 +162,158 @@ public class UserServiceTest {
 
         Assertions.assertEquals(
                 1,
-                greenMailExtension.getReceivedMessages().length
+                greenMailExtension
+                        .getReceivedMessages()
+                        .length
         );
     }
 
     @Test
     @Transactional
-    public void testVerifyUser() throws EmailFailureException {
-        Assertions.assertFalse(userService.verifyUser("Bad Token"), "Token that is bad or does not exist should return false.");
+    public void testVerifyUser()
+            throws EmailFailureException {
+
+        Assertions.assertFalse(
+                userService.verifyUser("Bad Token"),
+                "Token that is bad or does not exist should return false."
+        );
+
+        LocalUser unverifiedUser = localUserDao
+                .findByUsernameIgnoreCase("UserB")
+                .orElseThrow();
+
         LoginBody body = new LoginBody();
-        body.setUsername("UserB");
+        body.setUsername(unverifiedUser.getUsername());
         body.setPassword("PasswordB123");
-        try {
-            userService.loginUser(body);
 
-            Assertions.fail(
-                    "User should not have email verified."
-            );
-        } catch (UserNotVerifiedException ex) {
+        Assertions.assertThrows(
+                UserNotVerifiedException.class,
+                () -> userService.loginUser(body),
+                "User should not have email verified."
+        );
 
-            List<VerificationToken> tokens =
-                    verificationTokenDAO
-                            .findByUser_IdOrderByIdDesc(2L);
+        List<VerificationToken> tokens =
+                verificationTokenDAO
+                        .findByUser_IdOrderByIdDesc(
+                                unverifiedUser.getId()
+                        );
 
-            String token = tokens
-                    .getFirst()
-                    .getToken();
+        Assertions.assertFalse(
+                tokens.isEmpty(),
+                "A verification token should be created."
+        );
 
-            Assertions.assertTrue(
-                    userService.verifyUser(token),
-                    "Token should be valid."
-            );
+        String token = tokens
+                .getFirst()
+                .getToken();
 
-            LocalUser verifiedUser = localUserDao
-                    .findByUsernameIgnoreCase("UserB")
-                    .orElseThrow();
+        Assertions.assertTrue(
+                userService.verifyUser(token),
+                "Token should be valid."
+        );
 
-            Assertions.assertTrue(
-                    verifiedUser.isEmailVerified(),
-                    "The user should now be verified."
-            );
+        LocalUser verifiedUser = localUserDao
+                .findByUsernameIgnoreCase("UserB")
+                .orElseThrow();
 
-            List<VerificationToken> remainingTokens =
-                    verificationTokenDAO
-                            .findByUser_IdOrderByIdDesc(
-                                    verifiedUser.getId()
-                            );
+        Assertions.assertTrue(
+                verifiedUser.isEmailVerified(),
+                "The user should now be verified."
+        );
 
-            Assertions.assertTrue(
-                    remainingTokens.isEmpty(),
-                    "Verification tokens should be deleted after successful verification."
-            );
-        }
+        List<VerificationToken> remainingTokens =
+                verificationTokenDAO
+                        .findByUser_IdOrderByIdDesc(
+                                verifiedUser.getId()
+                        );
+
+        Assertions.assertTrue(
+                remainingTokens.isEmpty(),
+                "Verification tokens should be deleted after successful verification."
+        );
     }
 
     @Test
     @Transactional
-    public void testForgotPassword() throws MessagingException {
-        Assertions.assertThrows(EmailNotFoundException.class,
-                () -> userService.forgotPassword("UserNotExist@junit.com"));
-        Assertions.assertDoesNotThrow(() -> userService.forgotPassword("UserA@junit.com"),
-                "Non existing email should be rejected.");
-        Assertions.assertEquals("UserA@junit.com",
-                greenMailExtension.getReceivedMessages()[0]
-                        .getRecipients(Message.RecipientType.TO)[0].toString(), "Password " +
-                        "reset email should be sent.");
+    public void testForgotPassword()
+            throws MessagingException {
+
+        Assertions.assertThrows(
+                EmailNotFoundException.class,
+                () -> userService.forgotPassword(
+                        "UserNotExist@junit.com"
+                )
+        );
+
+        Assertions.assertDoesNotThrow(
+                () -> userService.forgotPassword(
+                        "UserA@junit.com"
+                ),
+                "Existing email should be accepted."
+        );
+
+        Assertions.assertEquals(
+                "UserA@junit.com",
+                greenMailExtension
+                        .getReceivedMessages()[0]
+                        .getRecipients(
+                                Message.RecipientType.TO
+                        )[0]
+                        .toString(),
+                "Password reset email should be sent."
+        );
     }
 
     @Test
-    public void testResetPassword() throws InvalidTokenException {
-        LocalUser user = localUserDao.findByUsernameIgnoreCase("UserA").get();
-        String token = jwtService.generatePasswordResetJWT(user);
-        PasswordResetBody body = new PasswordResetBody();
+    @Transactional
+    public void testResetPassword()
+            throws InvalidTokenException {
+
+        LocalUser user = localUserDao
+                .findByUsernameIgnoreCase("UserA")
+                .orElseThrow();
+
+        String token =
+                jwtService.generatePasswordResetJWT(user);
+
+        PasswordResetBody body =
+                new PasswordResetBody();
+
         body.setToken(token);
         body.setPassword("Password123456");
+
         userService.resetPassword(body);
-        user = localUserDao.findByUsernameIgnoreCase("UserA").get();
-        Assertions.assertTrue(encryptionService.verifyPassword("Password123456",
-                user.getPassword()), "Password change should be written to DB.");
+
+        LocalUser updatedUser = localUserDao
+                .findByUsernameIgnoreCase("UserA")
+                .orElseThrow();
+
+        Assertions.assertTrue(
+                encryptionService.verifyPassword(
+                        "Password123456",
+                        updatedUser.getPassword()
+                ),
+                "Password change should be written to DB."
+        );
     }
 
     @Test
+    @Transactional
     public void invalidPasswordThrowsInvalidCredentialsException() {
 
         LoginBody loginBody = new LoginBody();
 
         loginBody.setUsername("UserA");
-        loginBody.setPassword("DefinitelyWrongPassword123");
+        loginBody.setPassword(
+                "DefinitelyWrongPassword123"
+        );
 
         InvalidCredentialsException exception =
                 Assertions.assertThrows(
                         InvalidCredentialsException.class,
-                        () -> userService.loginUser(loginBody)
+                        () -> userService.loginUser(
+                                loginBody
+                        )
                 );
 
         Assertions.assertEquals(
@@ -221,16 +323,21 @@ public class UserServiceTest {
     }
 
     @Test
+    @Transactional
     public void unknownUsernameThrowsInvalidCredentialsException() {
 
         LoginBody loginBody = new LoginBody();
 
-        loginBody.setUsername("UserDoesNotExist");
+        loginBody.setUsername(
+                "UserDoesNotExist"
+        );
         loginBody.setPassword("Password123");
 
         Assertions.assertThrows(
                 InvalidCredentialsException.class,
-                () -> userService.loginUser(loginBody)
+                () -> userService.loginUser(
+                        loginBody
+                )
         );
     }
 }
