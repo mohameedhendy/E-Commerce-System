04055f146fe53862e2117b69013711ede8a8abcc
Mohamed Hendy <midohendy48@gmail.com>
2026-07-14 23:40:25 +0300

    refactor: optimize address and stock relationships

--- src/main/java/com/ecommerce/ecommerce_backend/model/Address.java
+++ src/main/java/com/ecommerce/ecommerce_backend/model/Address.java
@@ -1,12 +1,20 @@
 package com.ecommerce.ecommerce_backend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
 @Getter
+@Setter
 @Entity
 @Table(name = "address")
 public class Address {
@@ -16,21 +24,40 @@ public class Address {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "address_line_1", nullable = false, length = 512)
+    @Column(
+            name = "address_line_1",
+            nullable = false,
+            length = 512
+    )
     private String addressLine1;
 
-    @Column(name = "address_line_2", length = 512)
+    @Column(
+            name = "address_line_2",
+            length = 512
+    )
     private String addressLine2;
 
-    @Column(name = "country", nullable = false, length = 75)
+    @Column(
+            name = "country",
+            nullable = false,
+            length = 75
+    )
     private String country;
 
-    @Column(name = "city", nullable = false)
+    @Column(
+            name = "city",
+            nullable = false
+    )
     private String city;
 
     @JsonIgnore
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            optional = false
+    )
+    @JoinColumn(
+            name = "user_id",
+            nullable = false
+    )
     private LocalUser user;
-
 }
--- src/main/java/com/ecommerce/ecommerce_backend/model/Stock.java
+++ src/main/java/com/ecommerce/ecommerce_backend/model/Stock.java
@@ -1,7 +1,16 @@
 package com.ecommerce.ecommerce_backend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,21 +26,29 @@ public class Stock {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "quantity", nullable = false)
+    @Column(
+            name = "quantity",
+            nullable = false
+    )
     private Integer quantity;
 
     @Version
     @Setter(AccessLevel.NONE)
-    @Column(name = "version", nullable = false)
+    @Column(
+            name = "version",
+            nullable = false
+    )
     private Long version;
 
     @JsonIgnore
-    @OneToOne
+    @OneToOne(
+            fetch = FetchType.LAZY,
+            optional = false
+    )
     @JoinColumn(
             name = "product_id",
             nullable = false,
             unique = true
     )
     private Product product;
-
 }
